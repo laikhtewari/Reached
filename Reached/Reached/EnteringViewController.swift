512e61d08e0981b926b2a4d9d92731d8f7589d8c
Laikh Tewari <laikhtewari1@gmail.com>
2015-10-16 22:10:48 -0700

    CoreLocation.framework

---
+++ Reached/Reached/EnteringViewController.swift
@@ -0,0 +1,70 @@
+//
+//  EnteringViewController.swift
+//  Reached
+//
+//  Created by Laikh Tewari on 10/16/15.
+//  Copyright © 2015 Laikh Tewari. All rights reserved.
+//
+
+import UIKit
+
+class EnteringViewController: UIViewController {
+
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.addTarget(self, action: "didTapView")
+        self.view.addGestureRecognizer(tapRecognizer)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func goButtonClicked(sender: AnyObject)
+    {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://textbelt.com/text")!)
+        request.HTTPMethod = "POST"
+        let postString = "number=" + phoneTextField.text! + "&message=Hi muma"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            print("response = \(response)")
+            
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
+    
+    func didTapView()
+    {
+        self.view.endEditing(true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
